f0aa0f82ee46f9ac7b09e8f2fda1ee4a357a3215
Salva Mira <smira@iesperemaria.com>
2016-04-07 13:32:49 +0200

    Realitzat exercici 2.10

--- app/src/main/java/net/somtic/masviews/MainActivity.java
+++ app/src/main/java/net/somtic/masviews/MainActivity.java
@@ -3,17 +3,30 @@ package net.somtic.masviews;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText entrada;
+    private TextView salida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        entrada = (EditText)findViewById(R.id.entrada);
+        salida = (TextView)findViewById(R.id.salida);
+    }
+
+    public void sePulsa0(View view){
+        entrada.setText(entrada.getText() + (String)view.getTag());
     }
 
     public void sePulsa(View view){
-        Toast.makeText(this, "Pulsado", Toast.LENGTH_SHORT).show();
+        salida.setText(String.valueOf(
+                Float.parseFloat(entrada.getText().toString())*2.0));
     }
+
 }
